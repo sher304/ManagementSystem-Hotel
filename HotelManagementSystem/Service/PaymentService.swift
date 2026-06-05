59e41e4835ca240c539882b28cfe594a10b4a66a
sher304 <esherovshermat@gmail.com>
2026-06-05 19:17:07 +0200

    Payment Service done

--- HotelManagementSystem/Service/PaymentService.swift
+++ HotelManagementSystem/Service/PaymentService.swift
@@ -6,18 +6,48 @@
 //
 
 import Foundation
+import SwiftData
+
 
 class PaymentService {
-    // TODO: Complete methods
-    public func makeFullPayment() {
+    
+    private let repository: BaseRepository<Payment>
         
+    init(context: ModelContext) {
+        self.repository = BaseRepository<Payment>(context: context)
     }
     
-    public func makePartialPayment() {
-        
+    
+    @discardableResult
+    public func makeFullPayment(reservation: Reservation, amount: Double, method: String) -> Payment {
+        let payment = Payment(date: Date(),
+                              status: .paid, paymentType: method,
+                              paymentAmount: amount,
+                              reservation: reservation)
+        repository.add(payment)
+        print("Full payment of pln\(amount) processed via \(method).")
+        return payment
     }
     
-    public func makeRefund(amount: Double) {
+    @discardableResult
+    public func makePartialPayment(reservation: Reservation, amount: Double, method: String) -> Payment {
+        let payment = Payment(date: Date(),
+                              status: .pending, paymentType: method,
+                              paymentAmount: amount,
+                              reservation: reservation)
+        repository.add(payment)
+        print("Partial payment of pln\(amount) processed via \(method).")
+        return payment
+    }
+    
+    public func makeRefund(payment: Payment) {
+        if payment.status == .refunded {
+            print("Error: This payment has already been refunded.")
+            return
+        }
         
+        payment.status = .refunded
+        repository.saveChanges()
+        print("Refund of $\(payment.paymentAmount) has been successfully issued.")
     }
 }
